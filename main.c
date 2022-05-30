408fee124fd78d35ad25825b0df48eb58c67950f
trinhduymlo <trinhduymlo@gmail.com>
2022-05-30 20:01:17 +0700

    Bài tập 1

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void){
+	printf("Welcom to C!\n");
+
+	return 0;
+}
