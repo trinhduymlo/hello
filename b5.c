0b113ef67a726e5d6f6529ccca91e32859d17b35
trinhduymlo <trinhduymlo@gmail.com>
2022-06-01 18:41:57 +0700

    i

--- b5.c
+++ b5.c
@@ -4,9 +4,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int integer1;
-	int integer1;
+	int num1;
+	int num2;
 	int sum;
-	printf(Ënter first integer)
+	printf ("nhap num1 :\n");
+	scanf ("%d", &num1);
+	printf ("nhap num2 :\n");
+	scanf ("%d", &num2);
+	sum= num1 + num2;
+	printf ("bang tong: %d", sum);
 	return 0;
 }
